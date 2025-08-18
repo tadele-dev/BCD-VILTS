89e246c992b786fc2a183efe53d460d3f9e14d87
TADELE TATEK <59342309+tadele-dev@users.noreply.github.com>
2025-08-18 19:08:38 +0300

    Update BCDVILTSModel.py

--- BCDVILTSModel.py
+++ BCDVILTSModel.py
@@ -1,3 +1,8 @@
+# Copyright 2025 Tadele Tatek Gebrewold
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy at http://www.apache.org/licenses/LICENSE-2.0
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
